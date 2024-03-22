ce1a280caa79bbc172a8fac1b299cec7e80d7a93
ferapont <71552370+ferapontqiezi@users.noreply.github.com>
2024-03-22 21:31:52 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,6 @@
+from datasets.allweather import *
+from datasets.snow100k import *
+from datasets.outdoorrain import *
+from datasets.raindrop import *
+
+__all__ = ["AllWeather", "AllWeather1", "AllWeather2", "Snow100K", "OutdoorRain", "RainDrop"]
